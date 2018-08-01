2a537819c00300933e420c9943e94142e8a1d9a0
KazuhiroObama <obamakazuhiro@gmail.com>
2018-08-01 14:49:24 +0900

    メールで画像送信のための設定テスト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,11 +62,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "instagram_clone_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  # 投稿確認メールに画像を表示させる設定
-  config.action_controller.asset_host = 'https://sheltered-springs-61172.herokuapp.com'
-  config.action_mailer.asset_host = config.action_controller.asset_host
-  config.action_mailer.default_url_options = { host: 'sheltered-springs-61172.herokuapp.com' }
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -105,4 +100,7 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
+  # 投稿確認メールに画像を表示させる設定
+  config.action_controller.asset_host = 'https://sheltered-springs-61172.herokuapp.com'
+  config.action_mailer.asset_host = config.action_controller.asset_host
 end
