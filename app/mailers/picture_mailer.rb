9689ecf726e527cd97a26a3d2487baaa5f470170
KazuhiroObama <obamakazuhiro@gmail.com>
2018-08-01 14:08:08 +0900

    picture_mailer.rb edit

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -1,6 +1,6 @@
 class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
-    mail to: @picture.email, subject: "写真投稿完了のお知らせ"
+    mail to: @picture.user.email, subject: "写真投稿完了のお知らせ"
   end
 end
