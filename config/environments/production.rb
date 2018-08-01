3a77254dfe3fc9b4fae67f6ff855f13eb740d668
KazuhiroObama <obamakazuhiro@gmail.com>
2018-08-01 16:43:50 +0900

    instagram_clone

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,6 @@ Rails.application.configure do
 
   config.action_controller.asset_host = 'http://yourhostname.com'
   config.action_mailer.asset_host = config.action_controller.asset_host
-  config.action_mailer.default_url_options = { host: 'yourhostname.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
