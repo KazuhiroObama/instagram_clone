f75821d6b2e7ce627406daf3f04517baa63db207
KazuhiroObama <obamakazuhiro@gmail.com>
2018-08-06 20:53:04 +0900

    fix #6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,14 @@ module ApplicationHelper
       redirect_to @user
     end
   end
+
   def picture_correct_user
     if @picture.user != current_user
       flash["alert-danger"] = "権限がありません"
       redirect_to @user
     end
   end
+  
   def icon_correct_user
     if comment_params[:user_id] != current_user.id.to_s
       flash["alert-danger"] = "どうして他人のフリしてコメントするの？"
