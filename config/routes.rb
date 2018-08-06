ba859da975dcf3092b99441033b8091aa0f22420
KazuhiroObama <obamakazuhiro@gmail.com>
2018-08-06 19:04:46 +0900

    commentsに関するrouting変更。paramsでpicture_idを取得可能に。

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   root to: 'sessions#new'
   resources :users, except: :index
-  resources :pictures, except: :edit
-  resources :comments, only: [:create, :update, :destroy]
   get '/picture/favorites/:id', to: 'pictures#favorite', as: 'picture_favorites'
 
   resources :sessions, only: [:new, :create, :destroy]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :favorites, only: [:create, :destroy]
   resources :icons
+
+  resources :pictures, except: :edit do
+    resources :comments, only: [:create, :update, :destroy]
+  end
+
 end
